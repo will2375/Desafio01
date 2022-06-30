3817c716c88369eedc2326c2119c6b0743632ebb
will2375 <wil.liam_benicio@hotmail.com>
2022-06-30 20:36:17 -0300

    Desafio- quantidade entrega maxima

--- src/AdivinheONumero.java
+++ src/AdivinheONumero.java
@@ -16,19 +16,22 @@ public class AdivinheONumero {
             int numerod = input.nextInt();
             if (numerod == aleatorio) {
                 System.out.println("Parabens você acertou o numero ganhou 10 pontos ");
-                somaTotal = somaTotal+10;
-            } else if (numerod == aleatorio - 1 || numerod == aleatorio + 1) {
+                somaTotal = somaTotal + 10;
+            } else if (numerod > aleatorio - 1 || numerod < aleatorio - 1) {
+                System.out.println("você errou");
+            }
+            if (numerod == aleatorio - 1 || numerod == aleatorio + 1) {
                 System.out.println("muito proximo 5 pontos ");
-                somaTotal = somaTotal+5;
+                somaTotal = somaTotal + 5;
                 System.out.println(" tente novamente: ");
                 int segChanche = input.nextInt();
                 if (segChanche == aleatorio) {
                     System.out.println("Parabens você acertou e ganhou 10 pontos");
-                    System.out.println("Seus acertos foram: " + numerod + "," + segChanche );
-                    somaTotal = somaTotal+10;
+                    System.out.println("Seus acertos foram: " + numerod + "," + segChanche);
+                    somaTotal = somaTotal + 10;
                 } else {
                     System.out.println("Numero errado, você perdeu e zerou seus pontos ");
-                    System.out.println("numeros errados são: " + numerod + ", " + segChanche );
+                    System.out.println("numeros errados são: " + numerod + ", " + segChanche);
                 }
             }
             System.out.println("Deseja continuar jogando? S/N");
@@ -39,7 +42,7 @@ public class AdivinheONumero {
                 controlador = false;
             }
         }
-        System.out.println("Seus numeros errados foram: " );
+        System.out.println("Seus numeros errados foram: ");
         System.out.println("Sua pontuação total é de: " + somaTotal);
     }
 }
