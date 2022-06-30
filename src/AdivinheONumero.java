0341843cbc93261c9366ed94912533c509ccf1bf
will2375 <wil.liam_benicio@hotmail.com>
2022-06-29 23:00:11 -0300

    Desafio- quantidade minima para media

--- src/AdivinheONumero.java
+++ src/AdivinheONumero.java
@@ -1,5 +1,3 @@
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Scanner;
 import java.util.Random;
 
@@ -7,26 +5,38 @@ public class AdivinheONumero {
     public static void main(String[] args) {
 
         Scanner input = new Scanner(System.in);
-        System.out.print("Digite a quantidade de numeros que deseja jogar: ");
-        int quantidade = input.nextInt();
-        Random random = new Random();
-        int aleatorio = random.nextInt(quantidade);
-        System.out.println("Tente acertar o numero aleatorio dentro da quantidade digitada " + quantidade + ", digite um numero:");
-        int numerod = input.nextInt();
+        int somaTotal = 0;
+        boolean controlador = true;
+        while (controlador) {
 
-        if (numerod == aleatorio) {
-            System.out.println("Parabens você acertou o numero ganhou 10 pontos ");
+            System.out.print("Digite a quantidade de numeros que deseja jogar: ");
+            int quantidade = input.nextInt();
+            Random random = new Random();
+            int aleatorio = random.nextInt(quantidade);
+            System.out.println("Tente acertar o numero aleatorio dentro da quantidade digitada " + quantidade + ", digite um numero:");
+            int numerod = input.nextInt();
 
-        } else if (numerod == aleatorio - 1 || numerod == aleatorio + 1) {
-            System.out.println("muito proximo 5 pontos ");
-            System.out.println(" tente novamente: ");
-            int segChanche = input.nextInt();
-            if (segChanche == aleatorio) {
-                System.out.println("Parabens você acertou e acomulou 15 pontos");
-            } else {
-                System.out.println("Numero errado, você perdeu e zerou seus pontos ");
+            if (numerod == aleatorio) {
+                System.out.println("Parabens você acertou o numero ganhou 10 pontos ");
+                somaTotal = +10;
+            } else if (numerod == aleatorio - 1 || numerod == aleatorio + 1) {
+                System.out.println("muito proximo 5 pontos ");
+                somaTotal = +5;
+                System.out.println(" tente novamente: ");
+                int segChanche = input.nextInt();
+                if (segChanche == aleatorio) {
+                    System.out.println("Parabens você acertou e ganhou 10");
+                } else {
+                    System.out.println("Numero errado, você perdeu e zerou seus pontos ");
+                }
+            }
+            System.out.println("Deseja continuar jogando? S/N");
+            Scanner loop = new Scanner(System.in);
+            String opcao = input.next();
+            if (opcao.equalsIgnoreCase("n")) {
+                System.out.println("Jogo encerrado");
+                controlador = false;}
+                System.out.println("Sua pontuação total é de: "+ somaTotal);
             }
         }
-
     }
-}
